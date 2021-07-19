b9447d003a7f0cb6e7e60e57b8caa93792740d77
Sven Wappler <info@wappler.systems>
2021-07-19 18:19:54 +0200

    raising TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,5 @@
 <?php
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF['dashboard_in_toolbar'] = [
   'title' => 'Dashboard In Toolbar',
   'description' => 'Moves the Dashboard module to the toolbar.',
   'category' => 'misc',
@@ -22,28 +22,27 @@ $EM_CONF[$_EXTKEY] = array (
   'CGLcompliance' => '',
   'CGLcompliance_note' => '',
   'constraints' => 
-  array (
+  [
     'depends' => 
-    array (
-      'typo3' => '10.3.99-10.4.99',
+    [
+      'typo3' => '10.3.99-11.5.99',
       'php' => '7.1.0-7.4.99',
-    ),
+    ],
     'conflicts' => 
-    array (
-    ),
+    [
+    ],
     'suggests' => 
-    array (
-    ),
-  ),
-  '_md5_values_when_last_written' => '',
-  'suggests' => 
-  array (
-  ),
+    [
+    ],
+  ],
+  'suggests' =>
+  [
+  ],
   'autoload' => 
-  array (
+  [
     'psr-4' => 
-    array (
+    [
       'NamelessCoder\\DashboardInToolbar\\' => 'Classes/',
-    ),
-  )
-);
+    ],
+  ]
+];
